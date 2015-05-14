e067944d7e007ecb1ed9ee7bbcc8110df206d27d
Batuhan Çıkrıkcı <batuhancikrikci@gmail.com>
2015-05-15 01:27:53 +0300

    getUserName ve getUserID silindi

--- Helplines-war/src/main/java/com/helplines/util/Session.java
+++ Helplines-war/src/main/java/com/helplines/util/Session.java
@@ -18,20 +18,6 @@ public class Session {
 				.getExternalContext().getRequest();
 	}
 
-	public static String getUserName() {
-		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
-				.getExternalContext().getSession(false);
-		return session.getAttribute("userName").toString();
-	}
-
-	public static String getUserID() {
-		HttpSession session = getSession();
-		if (session != null)
-			return (String) session.getAttribute("userID");
-		else
-			return null;
-	}
-	
 	public static User getUser() {
 		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
 				.getExternalContext().getSession(false);
