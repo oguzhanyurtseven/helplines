bea43e636ae0cc9c9f349981795094d4d498bdee
Batuhan Çıkrıkcı <batuhancikrikci@gmail.com>
2015-05-18 14:10:21 +0300

    AuthenticationFilter eklendi

---
+++ Helplines-war/src/main/java/com/helplines/util/AuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.helplines.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(filterName = "AuthenticationFilter", urlPatterns = { "*.xhtml" })
+public class AuthenticationFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		try {
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpServletResponse res = (HttpServletResponse) response;
+			HttpSession ses = req.getSession(false);
+			String reqURI = req.getRequestURI();
+			if (reqURI.indexOf("/index.xhtml") >= 0
+					|| (ses != null && ses.getAttribute("user") != null))
+				chain.doFilter(request, response);
+			else
+
+				res.sendRedirect(req.getContextPath() + "/index.xhtml");
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
